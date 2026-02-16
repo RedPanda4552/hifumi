d9e6644b2d89ad3e0cdf1179d25370a980491fb5
RedPanda4552 <redpanda4552@gmail.com>
2026-02-15 21:43:22 -0500

    Add staff notification

--- src/main/java/net/pcsx2/hifumi/async/MaliciousForwardRunnable.java
+++ src/main/java/net/pcsx2/hifumi/async/MaliciousForwardRunnable.java
@@ -1,9 +1,16 @@
 package net.pcsx2.hifumi.async;
 
+import java.awt.Color;
+
+import org.apache.commons.lang3.StringUtils;
+
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageReference;
 import net.dv8tion.jda.api.entities.MessageReference.MessageReferenceType;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+import net.pcsx2.hifumi.HifumiBot;
 import net.pcsx2.hifumi.util.Messaging;
 
 public class MaliciousForwardRunnable implements Runnable
@@ -23,11 +30,22 @@ public class MaliciousForwardRunnable implements Runnable
 		if (ref != null && ref.getType() == MessageReferenceType.FORWARD)
 		{
 			Message forwardedMessage = ref.resolve().complete();
+			int componentCount = forwardedMessage.getComponents().size();
 			
-			if (!forwardedMessage.getComponents().isEmpty())
+			if (componentCount != 0)
 			{
 				this.message.delete().queue();
 				User user = this.message.getAuthor();
+				EmbedBuilder eb = new EmbedBuilder();
+				eb.setColor(Color.YELLOW);
+				eb.setTitle("Removed forwarded message containing interaction elements");
+				eb.setDescription(String.format("Forwarded message contained %d components, and was possibly being used to conceal a scam link.", componentCount));
+				eb.addField("User (as mention)", user.getAsMention(), true);
+				eb.addField("User ID", user.getId(), true);
+				eb.addField("Forwarded Message (Truncated to 512 chars)", StringUtils.abbreviate(forwardedMessage.getContentDisplay(), 512), false);
+				MessageCreateBuilder mb = new MessageCreateBuilder();
+				mb.addEmbeds(eb.build());
+				Messaging.sendMessage(HifumiBot.getSelf().getConfig().channels.systemOutputChannelId, mb.build());
 				Messaging.sendPrivateMessage(user, "Forwarding messages which contain buttons or other interactive items is not allowed for security reasons.");
 			}
 		}
